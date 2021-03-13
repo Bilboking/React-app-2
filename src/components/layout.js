13f8142fa46e02ba1724e976be538797d9af15c5
Bilboking <brkbendreality@gmail.com>
2021-03-13 09:46:43 -0600

    post build

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,12 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Helmet from 'react-helmet'
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout = ({ children}) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
